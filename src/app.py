22d59ae62a6842dac9f853fdf515cedb3ecc2ef7
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2021-09-20 21:24:59 +0000

    Creating Relationship one-to-one

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import db, User
+from models import db, User, Profile
 
 app = Flask(__name__)
 app.url_map.slashes = False
@@ -22,9 +22,94 @@ def main():
 @app.route('/api/users', methods=['GET'])
 def get_users():
     users = User.query.all()
-    users = list(map(lambda user: user.serialize(), users))
+    users = list(map(lambda user: user.serialize_with_profile(), users))
 
     return jsonify(users), 200
+
+
+@app.route('/api/users', methods=['POST'])
+def post_users():
+
+    name = request.json.get('name')
+    lastname = request.json.get('lastname')
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    bio = request.json.get('bio', "")
+    twitter = request.json.get('twitter', "")
+    facebook = request.json.get('facebook', "")
+    instagram = request.json.get('instagram', "")
+    linkedin = request.json.get('linkedin', "")
+
+    if not email: return jsonify({ "status": False, "msg": "Email is required!"}), 400
+
+    user = User.query.filter_by(email=email).first()
+    if user: return jsonify({ "status": False, "msg": "Email already in use!"}), 400
+
+    """ user = User()
+    user.name = name
+    user.lastname = lastname
+    user.email = email
+    user.password = password
+    user.save()
+
+    profile = Profile()
+    profile.bio = bio
+    profile.twitter = twitter
+    profile.facebook = facebook
+    profile.instagram = instagram
+    profile.linkedin = linkedin
+    profile.user_id = user.id
+    profile.save()
+    """
+
+    user = User()
+    user.name = name
+    user.lastname = lastname
+    user.email = email
+    user.password = password
+
+    profile = Profile()
+    profile.bio = bio
+    profile.twitter = twitter
+    profile.facebook = facebook
+    profile.instagram = instagram
+    profile.linkedin = linkedin
+
+    user.profile = profile # usando el relationship creado
+    user.save()
+
+
+    return jsonify(user.serialize_with_profile()), 201
+
+@app.route('/api/users/<int:id>', methods=['PUT'])
+def put_users(id):
+
+    name = request.json.get('name')
+    lastname = request.json.get('lastname')
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    user = User.query.get(id)
+    user.name = name
+    user.lastname = lastname
+    user.email = email
+    user.password = password
+    user.update()
+
+    return jsonify(user.serialize()), 200
+
+
+@app.route('/api/users/<int:id>', methods=['DELETE'])
+def delete_users(id):
+
+    user = User.query.get(id)
+
+    if not user: return jsonify({ "status": False, "msg": "User doesn't exists!"}), 404
+
+    user.delete()
+
+    return jsonify({ "status": True, "msg": "User deleted!"}), 200
 
 
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     lastname = db.Column(db.String(50))
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
+    profile = db.relationship('Profile', backref='user', uselist=False) # JOIN SQL
 
     def serialize(self):
         return {
@@ -18,6 +19,77 @@ class User(db.Model):
             "email": self.email
         }
 
+    def serialize_with_profile(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "email": self.email,
+            "profile": self.profile.serialize()
+        }
+
+        """
+        {
+            "id": 1,
+            "name": "",
+            "lastname": "",
+            "email": "",
+            "profile": {
+                "id": 1,
+                "bio": "",
+                "twitter": "",
+                "facebook": "",
+                "instagram": "",
+                "linkedin": ""
+            }
+        }
+        """
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    bio = db.Column(db.Text, default="")
+    twitter = db.Column(db.String(100), default="")
+    facebook = db.Column(db.String(100), default="")
+    instagram = db.Column(db.String(100), default="")
+    linkedin = db.Column(db.String(100), default="")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "bio": self.bio,
+            "twitter": self.twitter,
+            "facebook": self.facebook,
+            "instagram": self.instagram,
+            "linkedin": self.linkedin
+        }
+
+    def serialize_with_user(self):
+        return {
+            "id": self.id,
+            "name": self.user.name + " " + self.user.lastname, 
+            "bio": self.bio,
+            "twitter": self.twitter,
+            "facebook": self.facebook,
+            "instagram": self.instagram,
+            "linkedin": self.linkedin
+        }
+
+    
     def save(self):
         db.session.add(self)
         db.session.commit()
