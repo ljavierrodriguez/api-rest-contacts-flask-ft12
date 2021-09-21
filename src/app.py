8c9b713d26cba31e12c3773073b113a5007757eb
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2021-09-21 14:32:56 +0000

    Relationship One to Many

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import db, User, Profile
+from models import db, User, Profile, Contact
 
 app = Flask(__name__)
 app.url_map.slashes = False
@@ -21,11 +21,12 @@ def main():
 
 @app.route('/api/users', methods=['GET'])
 def get_users():
-    users = User.query.all()
-    users = list(map(lambda user: user.serialize_with_profile(), users))
+    users = User.query.all() # [<User 1>, <User 2>, <User 3>] Lista de Objetos de Python 
+    #print(users)
+    users = list(map(lambda user: user.serialize_with_profile(), users)) # [{"id": 1 }, {"id": 2}, {"id": 3}] Lista de Diccionarios de Python
+    #print(users)
 
     return jsonify(users), 200
-
 
 @app.route('/api/users', methods=['POST'])
 def post_users():
@@ -90,6 +91,13 @@ def put_users(id):
     email = request.json.get('email')
     password = request.json.get('password')
 
+    bio = request.json.get('bio', "")
+    twitter = request.json.get('twitter', "")
+    facebook = request.json.get('facebook', "")
+    instagram = request.json.get('instagram', "")
+    linkedin = request.json.get('linkedin', "")
+
+    """     
     user = User.query.get(id)
     user.name = name
     user.lastname = lastname
@@ -97,8 +105,30 @@ def put_users(id):
     user.password = password
     user.update()
 
-    return jsonify(user.serialize()), 200
+    profile = Profile.query.filter_by(user_id=user.id).first()
+    profile.bio = bio
+    profile.twitter = twitter
+    profile.facebook = facebook
+    profile.instagram = instagram
+    profile.linkedin = linkedin
+    profile.update()
+    """
 
+    user = User.query.get(id)
+    user.name = name
+    user.lastname = lastname
+    user.email = email
+    user.password = password
+    user.profile.bio = bio
+    user.profile.twitter = twitter
+    user.profile.facebook = facebook
+    user.profile.instagram = instagram
+    user.profile.linkedin = linkedin
+    user.update()
+
+    
+
+    return jsonify(user.serialize_with_profile()), 200
 
 @app.route('/api/users/<int:id>', methods=['DELETE'])
 def delete_users(id):
@@ -110,6 +140,56 @@ def delete_users(id):
     user.delete()
 
     return jsonify({ "status": True, "msg": "User deleted!"}), 200
+
+
+
+@app.route("/api/user/<int:user_id>/contacts", methods=['GET', 'POST'])
+@app.route("/api/user/<int:user_id>/contacts/<int:contact_id>", methods=['GET', 'PUT', 'DELETE'])
+def contacts_by_user(user_id, contact_id = None):
+    if request.method == 'GET':
+        if contact_id is not None:
+            contact = Contact.query.filter_by(user_id=user_id, id=contact_id).first()
+            if not contact: return jsonify({ "status": False, "msg": "Contact not found!"}), 404
+            return jsonify(contact.serialize()), 200
+        else:
+            contacts = Contact.query.filter_by(user_id=user_id)
+            contacts = list(map(lambda contact: contact.serialize(), contacts))
+            return jsonify(contacts), 200
+
+    if request.method == 'POST':
+
+        name = request.json.get("name")
+        phone_work = request.json.get("phone_work")
+        phone_home = request.json.get("phone_home", "")
+        email = request.json.get("email", "")
+
+        contact = Contact()
+        contact.name = name
+        contact.phone_work = phone_work
+        contact.phone_home = phone_home
+        contact.email = email
+        contact.user_id = user_id
+        contact.save()
+
+        return jsonify(contact.serialize()), 201
+
+    if request.method == 'PUT':
+
+        name = request.json.get("name")
+        phone_work = request.json.get("phone_work")
+        phone_home = request.json.get("phone_home", "")
+        email = request.json.get("email", "")
+
+        contact = Contact.query.filter_by(user_id=user_id, id=contact_id).first()
+        if not contact: return jsonify({ "status": False, "msg": "Contact not found!"}), 404
+
+        contact.name = name
+        contact.phone_work = phone_work
+        contact.phone_home = phone_home
+        contact.email = email
+        contact.update()
+
+        return jsonify(contact.serialize()), 200
 
 
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,7 +9,8 @@ class User(db.Model):
     lastname = db.Column(db.String(50))
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
-    profile = db.relationship('Profile', backref='user', uselist=False) # JOIN SQL
+    profile = db.relationship('Profile', cascade="all, delete", backref='user', uselist=False) # JOIN SQL ONE TO ONE
+    contacts = db.relationship('Contact', cascade="all, delete", backref="user") # JOIN SQL ONE TO MANY
 
     def serialize(self):
         return {
@@ -25,7 +26,8 @@ class User(db.Model):
             "name": self.name,
             "lastname": self.lastname,
             "email": self.email,
-            "profile": self.profile.serialize()
+            "profile": self.profile.serialize(),
+            "contacts": self.get_contacts() 
         }
 
         """
@@ -44,6 +46,9 @@ class User(db.Model):
             }
         }
         """
+
+    def get_contacts(self):
+        return list(map(lambda contact: contact.serialize(), self.contacts))
 
 
     def save(self):
@@ -66,7 +71,7 @@ class Profile(db.Model):
     facebook = db.Column(db.String(100), default="")
     instagram = db.Column(db.String(100), default="")
     linkedin = db.Column(db.String(100), default="")
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
 
     def serialize(self):
         return {
@@ -87,6 +92,38 @@ class Profile(db.Model):
             "facebook": self.facebook,
             "instagram": self.instagram,
             "linkedin": self.linkedin
+        }
+
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Contact(db.Model):
+    __tablename__ = 'contacts'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    phone_work = db.Column(db.String(200), nullable=False)
+    phone_home = db.Column(db.String(200), default="")
+    email = db.Column(db.String(100), default="")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "phone_work": self.phone_work,
+            "phone_home": self.phone_home,
+            "email": self.email
         }
 
     
